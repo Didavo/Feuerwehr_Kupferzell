698e0cb96c9d7ecd2801140f3bf889df08ec6a62
kevin <kevinbeck200@web.de>
2020-09-21 16:04:38 +0200

    commit

--- app/__init__.py
+++ app/__init__.py
@@ -22,17 +22,9 @@ def create_app():
     db.init_app(app)
     bootstrap.init_app(app)
     login_manager.init_app(app)
-    print("die globals sind", globals())
-
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-
-    # from .auth import auth as auth_blueprint
-    # app.register_blueprint(auth_blueprint, url_prefix='/auth')
-
-    # from .api import api as api_blueprint
-    # app.register_blueprint(api_blueprint, url_prefix='/api/v1')
 
 
     # if app.config['SSL_REDIDRECT']:
